3d8486803bea52f8c9902f44f5f1e3b55b4f54e6
JAGicana-43 <jagicana@spamast.edu.ph>
2026-02-14 22:44:20 +0800

    Add files via upload

---
+++ Trivia_Bot_GICANA.js
@@ -0,0 +1,23 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "John Aaron "Erzo" Gicana;
+let botLocation = "the Console";
+let favoriteLanguage = "JavaScript";
+
+console.log(`My name is ${botName} and I live on ${botLocation}.`);
+
+console.log(`My favorite programming language is ${favoriteLanguage}.`);
+
+let codingFact = `${favoriteLanguage} was created in just 10 days!`;
+
+console.log(codingFact);
+
+codingFact = `${favoriteLanguage} is one of the most popular programming languages in the world.`;
+
+console.log(codingFact);
+
+codingFact = `With ${favoriteLanguage}, you can build websites, servers, and even mobile apps!`;
+
+console.log(codingFact);
+
+console.log(`It was fun sharing these facts with you. Goodbye! - ${botName} from ${botLocation}.`);
